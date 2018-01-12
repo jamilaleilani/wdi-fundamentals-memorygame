abda7963ff93b4037ed04d220142a9cd1f22a9af
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-01-11 22:02:35 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,25 @@
-var cardOne = 'queen';
 
-var cardTwo = 'queen';
 
-var cardThree = 'king';
+var cards = ["queen", "queen", "king", "king"];
 
-var cardFour = 'king';
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped a " + cardsInPlay);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped a " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Try again.");
+	}
+}
