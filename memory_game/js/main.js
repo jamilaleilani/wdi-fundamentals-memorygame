fc097c32ba0566b9c21f5906a634a70e260f9f1e
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-01-15 17:08:05 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -36,13 +36,34 @@ var checkForMatch = function () {
 	}
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute("src", cards[cardId].cardImage);
 	console.log("User flipped a " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 
 };
 
-flipCard(0);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", 'images/back.png');
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	};
+};
 
-flipCard(2);
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
