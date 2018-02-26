057cf4c071edcd4b4a9ff74152d6263f81db5998
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-02-26 12:17:55 -0800

    removed DS_Store

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,25 @@
 
 
 var cards = [
-	{ 
+	{
 		rank: "queen",
 		suit: "hearts",
 		cardImage: "images/queen-of-hearts.png"
 	},
 
-	{ 
+	{
 		rank: "queen",
 		suit: "diamonds",
 		cardImage: "images/queen-of-diamonds.png"
 	},
 
-	{ 
+	{
 		rank: "king",
 		suit: "hearts",
 		cardImage: "images/king-of-hearts.png"
 	},
 
-		{ 
+		{
 		rank: "king",
 		suit: "diamonds",
 		cardImage: "images/king-of-diamonds.png"
@@ -57,8 +57,6 @@ var createBoard = function () {
 
 
 createBoard();
-
-
 
 
 
